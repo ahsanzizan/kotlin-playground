633422b40c841466ffac2acf8498d580fba7232b
Ahsan <ahsanaz461@gmail.com>
2023-10-17 08:41:34 +0700

    add PyramidStars.kt

---
+++ src/main/kotlin/PyramidStars.kt
@@ -0,0 +1,10 @@
+// Good Old Pyramid Stars
+fun main() {
+    val x: Int = 5
+
+    for (i in 0 until x) {
+        repeat(x - i) { print(" ") }
+        repeat(i * 2 + 1) { print("*") }
+        println()
+    }
+}
